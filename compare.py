a251f0257e202a69f6dfa3cd5ae5925c5f25ef95
takapt <takapt@a.com>
2015-05-29 17:56:15 +0900

    lose prob

--- compare.py
+++ compare.py
@@ -16,10 +16,13 @@ p, q = get_scores(a), get_scores(b)
 seeds = list(set(p) & set(q))
 sum_ratio = 0
 win_p = 0
+lose_p = 0
 for seed in seeds:
     x, y = p[seed], q[seed]
     if x < y:
         win_p += 1
+    elif x > y:
+        lose_p += 1
 
     ratio = y / (x + 1e-10)
     if ratio < 20:
@@ -29,5 +32,7 @@ for seed in seeds:
 
 total_ratio = sum_ratio / len(seeds)
 win_p /= len(seeds)
+lose_p /= len(seeds)
 print('total_ratio: {}'.format(total_ratio))
 print('win_p: {}'.format(win_p))
+print('lose_p: {}'.format(lose_p))
